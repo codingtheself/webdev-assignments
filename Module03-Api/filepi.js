88a96dfb2c784f6cd622c4785bca869d60fef79d
codingtheself <theneway11@gmail.com>
2025-02-05 17:13:30 +0530

    added more features to filepi.js

--- Module03-Api/filepi.js
+++ Module03-Api/filepi.js
@@ -31,6 +31,81 @@ app.get('/listall/:fpath/*', (req, res) => {
     });
 });
 
+
+// GET for creating file
+
+app.get('/createfile/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const fpath = './' + filename;
+
+    fs.open(fpath, "w", (err) => {
+        if(err) throw err;
+
+        res.send(`${filename} created successfully!`);
+    });
+});
+
+// GET for reading the file content
+
+app.get('/readfile/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const fpath = './' + filename;
+
+    fs.readFile(fpath, 'utf-8', (err, data) => {
+        if(err) throw err;
+
+        res.json(data);
+    });
+});
+
+app.get("/readfile/:fpath/*", (req, res) => {
+  console.log(req.params);
+
+  const fpath = req.params.fpath + "/" + req.params["0"];
+  console.log(fpath);
+
+  fs.readFile(fpath, 'utf-8',(err, data) => {
+    res.json(data);
+  });
+});
+
+// GET for deleting a file and folder
+
+app.get("/deletefile/:filename", (req, res) => {
+  const filename = req.params.filename;
+  const fpath = "./" + filename;
+
+  fs.rm(fpath, (err) => {
+    if(err) throw err;
+
+    res.send(`${filename} deleted successfully!`);
+  });
+});
+
+app.get("/deletedir/:dirname", (req, res) => {
+  const dirname = req.params.dirname;
+  const fpath = "./" + dirname;
+
+  fs.rmdir(fpath, (err) => {
+    if(err) throw err;
+
+    res.send(`${dirname} is deleted successfully!`);
+  });
+});
+
+app.get("/deletedir/:fpath/*", (req, res) => {
+  console.log(req.params);
+
+  const fpath = req.params.fpath + "/" + req.params["0"];
+  console.log(fpath);
+
+  fs.rmdir(fpath, (err) => {
+    if(err) throw err;
+
+    res.send(`[Deleted] ${fpath}`);
+  });
+});
+
 app.listen(3000, () => {
     console.log(`server listening on https://localhost:3000/`);
 });
--- Module03-Api/readme.md
+++ Module03-Api/readme.md
@@ -6,4 +6,13 @@
     - **Some Bugs:**
         - not putting `"/"` after the route while sending request for the directory *generates error*.
 
-- [ ] accept *GET* request `/createfile:filename/`. Which will create a file in with filename given in the params.
+- [x] accept *GET* request `/createfile:filename/`. Which will create a file in with filename given in the params.
+    - Still I need to add the features of actually passing some content to write to the file
+    - It also *does not check if file exists, it overrides*
+
+- [x] added *GET* request `/deletefile/:filename/` for deleting files and `/deletedir/:dirname/` for deleting folders
+    - **Some Bugs**
+        - If the folder has subfolders then, it throws an error and cannot delete the folders unless all the subfolders are deleted first!
+
+- [x] added *GET* request `/readfile/:filename` for reading the content of the file in *UTF-8* encoding
+
